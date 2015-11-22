6e29496a2d2af90827cd619be5a7abb9503ad739
John Beil <jbeil@mac.com>
2015-11-22 08:27:49 -0800

    added path to database in config. create database table.

--- buoybot.go
+++ buoybot.go
@@ -56,6 +56,7 @@ type Config struct {
 func main() {
 	fmt.Println("Starting BuoyBot...")
 
+	// load configuration
 	config := Config{}
 	loadConfig(&config)
 
---
+++ observations.sql
@@ -0,0 +1,3 @@
+-- PREPARE DATABASE
+
+CREATE TABLE Observations (id integer, date text, windspeed real, significantwaveheight real, dominantwaveperiod integer, averageperiod real, meanwavedirection text, airtemperature real, watertemperature real);

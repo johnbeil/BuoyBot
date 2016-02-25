dc72dddfcaf6a95b5b64181a0768250bb3f1331d
John Beil <jbeil@mac.com>
2016-02-24 21:02:29 -0800

    added sql queries for largest wave observations

--- observations.sql
+++ observations.sql
@@ -19,3 +19,9 @@ ALTER TABLE observations
 
 -- Extract CSV
 \copy (SELECT observationtime, significantwaveheight, dominantwaveperiod, averageperiod, airtemperature, watertemperature FROM observations) TO data.csv CSV DELIMITER ',';
+
+-- Select Max wave height ever
+SELECT * FROM observations ORDER BY significantwaveheight DESC LIMIT 1;
+
+-- Select Max wave height in given year
+SELECT * FROM observations WHERE EXTRACT(year FROM "rowcreated") = 2016 ORDER BY significantwaveheight DESC LIMIT 1;
